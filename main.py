a983775a592526adcb5c95a277d19ffc385ad4bb
Ram velagala <67812401+ramvelagala@users.noreply.github.com>
2021-02-26 14:51:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,18 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+SELECT  
+ 
+   attname       AS COL_NAME
+FROM _v_table a
+   JOIN _v_relation_column b
+   ON a.objid   = b.objid
+WHERE a.tablename = 'M_CPF_FC_MO_CNSMR_LOAN_ARGT_DTL'
+AND COL_NAME LIKE '%_SK'
+AND a.schema = 'ADMIN'
+ORDER BY attnum;  
+ 
+ 
+2)
+Select COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = " " and TABLE_SCHEMA = " "
