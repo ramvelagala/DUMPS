23141cef09f81a169d07d3b04157fd0d35545db3
Ram velagala <67812401+ramvelagala@users.noreply.github.com>
2021-03-04 18:18:55 -0600

    Update neg_num_report.py
    adfsaaasdx

--- neg_num_report.py
+++ neg_num_report.py
@@ -1,233 +1,308 @@
-from .data_elements_query import DataQuery,data_main,data_meta
+{
+    "name": "Report",
 
+    "type": "record",
 
+    "fields": [
+        {
+            "name": "Column",
+            "type": "string",
+            "doc": "name of the column.",
+            "default": " "
+        },
+        {
+            "name": "inferred_data_class",
+            "type": "string",
+            "doc" : "inferred data class of the column."
 
-def schema():
+        },
 
-    query_data = DataQuery(data_main, data_meta)
-    gen_format_items = query_data.format_generation()
-
-    data = {
-        "name": "Report",
-        "properties":{
-                "host_name": query_data.host_name,
-                "Data Store Name": "",
-                "Database Name": query_data.db_name,
-                "Table Name": query_data.table_name
-            },
-
-        "type": "record",
-
-        "fields": [
-            {
-                "name": "Column",
-                "type": "string",
-                "values": "",
-            },
-            {
-                "name": "inferred_data_class",
-                "type": "string",
-                "values": "",
-            },
-
-#metadata
-            {
+        {
+            "name": "MetaData",
+            "type": {
                 "name": "MetaData",
-                "type": {
-                    "name": "MetaData",
-                    "type": "record",
-                    "fields": [
-                        {
+                "type": "record",
+                "fields": [{
+                    "name": "Defined",
+                    "type": {
+                        "name": "Defined",
+                        "type": "record",
+                        "doc": "Meta data that is defined on column level.",
+                        "fields": [
+                            {
+                                "name": "Data_type",
+                                "type": "string",
+                                "doc": ""
+
+                            },
+                            {
+                                "name": "data_length",
+                                "type": "int",
+                                "doc": ""
+
+                            },
+                            {
+                                "name": "Precision",
+                                "type": "int",
+                                "doc": ""
+
+                            },
+                            {
+                                "name": "scale",
+                                "type": "int"
+
+                            },
+                            {
+                                "name": "data_scale",
+                                "type": "int",
+                                "doc": ""
+
+                            }
+                            ]
+                    }
+                },
+
+                    {
+                        "name": "Inferred",
+                        "type": {
                             "name": "Inferred",
-                            "type": {
-                                "name": "Inferred",
-                                "type": "record",
-                                "fields": [
-                                    {
-                                        "name": "Data_type",
-                                        "type": "string",
-                                        "values": "",
-                                    },
-                                    {
-                                        "name": "data_length",
-                                        "type": "int",
-                                        "values": "",
-                                    },
-                                    {
-                                        "name": "Precision",
-                                        "type": "int",
-                                        "values": query_data.num_prec,
-                                    },
-                                    {
-                                        "name": "scale",
-                                        "type": "int",
-                                        "values": "",
-                                    },
-                                    {
-                                        "name": "data_scale",
-                                        "type": "int",
-                                        "values": "",
-                                    }
-                                ]
+                            "type": "record",
+                            "doc": "Meta data that we have in column level.",
+                            "fields": [
+                            {
+                                "name": "Data_type",
+                                "type": "string",
+                                "doc": ""
+
+                            },
+                            {
+                                "name": "data_length",
+                                "type": "int",
+                                "doc": ""
+
+                            },
+                            {
+                                "name": "Precision",
+                                "type": "int",
+                                "doc": ""
+
+                            },
+                            {
+                                "name": "scale",
+                                "type": "int",
+                                "doc": ""
+
+                            },
+                            {
+                                "name": "data_scale",
+                                "type": "int",
+                                "doc": ""
+
                             }
-                        },
-                        {
-                            "name": "Defined",
-                            "type": {
-                                "name": "Defined",
-                                "type": "record",
-                                "fields": [
-                                    {
-                                        "name": "Data_type",
-                                        "type": "string",
-                                        "values": "",
-                                    },
-                                    {
-                                        "name": "data_lenght",
-                                        "type": "int"
-                                    },
-                                    {
-                                        "name": "Precision",
-                                        "type": "int"
-                                    },
-                                    {
-                                        "name": "scale",
-                                        "type": "int"
-                                    },
-                                    {
-                                        "name": "data_scale",
-                                        "type": "int"
-                                    }
-                                ]
-                            }
-                        },
-                        {
-                            "name": "Defined",
-                            "type": {
-                                "name": "Defined",
-                                "type": "record"
-                            }
+                            ]
                         }
-                    ]
-                }
-            },
-# Domain Higlights
+                    }
+                ]
+            }
+        },
+        {
+            "name": "domain_highlights",
+            "type":
+            [
             {
                 "name": "domain_highlights",
-                "type": {
-                    "name": "domain_highlights",
-                    "type": "record",
-                    "fields": [
-                        {
-                            "name": "TotalRows",
-                            "type": "int",
-                            "values": query_data.total_rows,
-                        },
-                        {
-                            "name": "Cardinality",
-                            "type": "int",
-                            "values": query_data.cardinality,
-                        },
-                        {
-                            "name": "per_of_Cardinality",
-                            "type": "int",
-                            "values": query_data.perc_of_cardinality,
-                        },
-                        {
-                            "name": "Nulls",
-                            "type": "int",
-                            "values": query_data.num_null_values,
-                        },
-                        {
-                            "name": "no_of_general_formats",
-                            "type": "int",
-                            "values": len(gen_format_items),
-                        },
-
-                    ]
-                },
-                "properties": {
-
-                    {
-                        "name": "general_formats",
-                        "type": "array",
-                        "values": gen_format_items,
-
-
-                    },
-                    {
-                        "name": "Lowest Value",
-                        "type": "string",
-                        "values": query_data.lowest_value,
-                    },
-                    {
-                        "name": "Highest Value",
-                        "type": "string",
-                        "values": query_data.highest_value,
-                    },
-                    {
-                        "name": "Median Value",
-                        "type": "string",
-                        "values": query_data.median_value,
-                    },
-                    {
-                        "name": "Least Frequent Value",
-                        "type": "string",
-                        "values": query_data.freq_lowest_value,
-                    },
-                    {
-                        "name": "Lowest Frequency",
-                        "type": "string",
-                        "values": query_data.freq_lowest_count,
-                    },
-                    {
-                        "name": "Lowest Frequency percentage",
-                        "type": "string",
-                        "values": query_data.freq_lowest_perc,
-                    },
-                    {
-                        "name": "Highest Frequent Value",
-                        "type": "string",
-                        "values": query_data.freq_highest_value_value,
-                    },
-                    {
-                        "name": "Highest Frequency",
-                        "type": "string",
-                        "values": query_data.freq_highest_count_count,
-                    },
-                    {
-                        "name": "highest Frequency percentage",
-                        "type": "string",
-                        "values": query_data.freq_highest_perc,
-                    },
-
-                }
-            },
-
-            {
-                "name": "domain_values",
-                "type": "array",
+                "type": "record",
+                "doc": "Contains all the aggregations in the report.",
                 "fields": [
                     {
-                        "name": "top_25",
-                        "type": "array",
-                        "value": [],
+                        "name": "TotalRows",
+                        "type": "int",
+                        "doc": "Total number of rows in the column."
+
                     },
                     {
-                        "name": "bottom_25",
-                        "type": "array",
-                        "value": [],
+                        "name": "Cardinality",
+                        "type": "int",
+                        "doc": "Number of unique/distinct values in the column."
+
+                    },
+                    {
+                        "name": "per_of_Cardinality",
+                        "type": "float",
+                        "doc": "Percentage of unique values in the column."
+
+
+                    },
+                    {
+                        "name": "Nulls",
+                        "type": "int",
+                        "doc": "Number of nulls in the column."
+
+                    },
+                    {
+                        "name": "no_of_general_formats",
+                        "type": "int",
+                        "items": "string",
+                        "doc": "Total number of unique formats in column."
+
+                    },
+                    {
+                    "name": "general_formats",
+                    "doc": "List of different types of general formats in the column.",
+                    "type":
+					{
+						"type": "array",
+						"items": "string",
+						"name": "general_formats"
+					}
+
+
+                    },
+                    {
+                        "name": "Lowest_Value",
+                        "type": "string",
+                        "doc": "Lowest value in the column."
+
+                    },
+                    {
+                        "name": "Highest_Value",
+                        "type": "string",
+                        "doc": "Highest value in the column."
+
+                    },
+                    {
+                        "name": "Median_Value",
+                        "type": "string",
+                        "doc": "Median value in the column."
+
+                    },
+                    {
+                        "name": "Least_Frequent_Value",
+                        "type": "string",
+                        "doc": "Least frequent value in the column."
+
+                    },
+                    {
+                        "name": "Lowest_Frequency",
+                        "type": "int",
+                        "doc": "frequency of the lowest frequency value in the column."
+
+                    },
+                    {
+                        "name": "Lowest_Frequency_percentage",
+                        "type": "float",
+                        "doc": "percentage of frequency of the lowest frequency value in the column."
+
+                    },
+                    {
+                        "name": "Highest_Frequent_Value",
+                        "type": "int",
+                        "doc": "Highest frequent value in the column."
+
+                    },
+                    {
+                        "name": "Highest_Frequency",
+                        "type": "int",
+                        "doc": "frequency of the lowest frequency value in the column."
+
+                    },
+                    {
+                        "name": "highest_Frequency_percentage",
+                        "type": "float",
+                        "doc": "percentage of frequency of the lowest frequency value in the column."
+
                     }
-                ],
 
-            },
+                ]
+            }
+            ]
 
-        ]
-    }
+        },
+		{
+            "name": "domain_values",
+            "type":
+            [
+            {
+                "name": "domain_values",
+                "type": "record",
+                "fields":
+				[
+				{
+
+                    "name": "bot_25",
+                    "doc": "bot 25 % (quarter) of distinct values in array",
+                    "type":
+						{
+							"type": "array",
+							"items":
+							    {
+							        "type": "record",
+                                    "name": "bot_25_perc_unqiue",
+                                    "fields":
+                                    [
+                                    {
+                                        "name": "bot_25_unique_values",
+                                        "doc": "bot 25 % (quarter) of distinct values in array.",
+                                        "type": "double"
+                                    },
+                                    {
+                                        "name": "freq_bot_25_unique_values",
+                                        "doc": "Frequency of bot 25 % (quarter) of distinct values in array.",
+                                        "type": "int"
+                                    },
+
+                                    {
+                                        "name": "perc_freq_bot_25_unique_values",
+                                        "doc": "Percentage of frequency of bot 25 % (quarter) of distinct values in array.",
+                                        "type": "float"
+                                    }
+                                    ]
+							    }
+						}
+
+				},
+				{
+
+                    "name": "top_25",
+                    "doc": "top 25 % (quarter) of distinct values in array",
+                    "type":
+						{
+							"type": "array",
+							"items":
+							    {
+							        "type": "record",
+                                    "name": "top_25_perc_unqiue",
+                                    "fields":
+                                    [
+                                    {
+                                        "name": "top_25_unique_values",
+                                        "doc": "top 25 % (quarter) of distinct values in array.",
+                                        "type": "double"
+                                    },
+                                    {
+                                        "name": "freq_top_25_unique_values",
+                                        "doc": "Frequency of top 25 % (quarter) of distinct values in array.",
+                                        "type": "int"
+                                    },
+
+                                    {
+                                        "name": "perc_freq_top_25_unique_values",
+                                        "doc": "Percentage of frequency of top 25 % (quarter) of distinct values in array.",
+                                        "type": "float"
+                                    }
+                                    ]
+							    }
+						}
+
+				}
 
 
-    return data
+				]
+
+			}
+			]
+		}
 
 
-
-print(schema())
+    ]
+}
