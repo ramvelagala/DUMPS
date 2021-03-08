440f05255db2c10a3df7a926307daa0e0c460a42
Ram velagala <67812401+ramvelagala@users.noreply.github.com>
2021-03-08 08:50:48 -0600

    Update main.py
    asfasfaxaxasx

--- main.py
+++ main.py
@@ -1,23 +1,62 @@
-"""Avro Schema Parsing and dump to JSON."""
 
-
+import copy
 import json
-import pathlib
-import avro.schema
+import avro
+from avro.datafile import DataFileWriter, DataFileReader
+from avro.io import DatumWriter, DatumReader
+
+def avro_gen():
 
 
-file = pathlib.Path(__file__).parent / 'asset' / 'schema.avsc'
-dir_path = pathlib.Path(__file__).parent / 'asset'
+    schema = {
+      "type" : "record",
+      "name" : "MyClass",
+      "namespace" : "com.test.avro",
+      "fields" : [
+          {
+            "name" : "tablename",
+            "type" : "string"
+            },
+          {
+            "name" : "elements",
+            "type" : {
+              "type" : "array",
+              "items" : "string"
+            }
+            }
+        ]
+    }
+
+    data = {'tablename': 'Pierre-Simon Laplace', 'elements': ['col2', 'dfdfd']}
 
 
-def read_avsc(filepath):
-    """Read avro schema and return avro object.
+    # Parse the schema so we can use it to write the data
+    schema_parsed = avro.schema.parse(json.dumps(schema))
 
-    return avro schema
-    """
-    schema = avro.schema.parse(open(filepath / "_sk_result.avsc", "rb").read())
-    return schema
+    # Write data to an avro file
+    with open('C:\\Users\\schitturi\\Desktop\\swamy\\users.avro', 'wb') as f:
+        writer = DataFileWriter(f, DatumWriter(), schema_parsed)
+        writer.append({'tablename': 'Pierre-Simon Laplace', 'elements': list(['col2', 'dfdfd'])})
+        # writer.append({'tablename': 'DAta Profile', 'elements': list(['some_sk', 'few_sk'])})
+        writer.close()
+    with open('C:\\Users\\schitturi\\Desktop\\swamy\\users.avro', 'rb') as f:
+        reader = DataFileReader(f, DatumReader())
+        users = [user for user in reader]
+        reader.close()
+
+    # print(f'Users:\n {users}')
+    # dictionaries comparision
+    # print(cmp(data, users))
+    if users[0] == data:
+        print("its true")
+    else:
+        print("not;;;;;;;;;;;;;")
+    # print(result)
+    print(users[0])
+    print(data)
+
+    return [data, users]
 
 
-data = read_avsc(dir_path)
-print(json.dumps(data.to_json(), separators=(':', ',')))
+
+avro_gen()
