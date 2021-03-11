0a7a2e584ee112e89117dd9ee3ee4b393ac1e473
Ram velagala <67812401+ramvelagala@users.noreply.github.com>
2021-03-10 22:54:01 -0600

    Update main.py
    asdaszxaszx

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ WHERE p2.AGILEGROUP >= p1.AGILEGROUP ) <=
 GROUP BY p1.AGILEGROUP
 DESC
 ;
+
+
+
+
+
+SELECT  count($column) as minus_count, count(*) as count1, minus_count|count1 as percent
+ FROM $table
+Where  $column = -1
