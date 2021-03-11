cc8e8746c9624839bf7f7c3f2570ee6b09c3eb44
Ram velagala <67812401+ramvelagala@users.noreply.github.com>
2021-03-11 11:46:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-SELECT DISTINCT AGILEGROUP, COUNT(AGILEGROUP) as frequ
-FROM DBKDMDB.ADMIN.AGILELABBERS p1
-WHERE ((SELECT  count(*) FROM DBKDMDB.ADMIN.AGILELABBERS p2
-WHERE p2.AGILEGROUP >= p1.AGILEGROUP ) <=
-(SELECT  0.25 * count(*) FROM DBKDMDB.ADMIN.AGILELABBERS) )
-GROUP BY p1.AGILEGROUP
-DESC
-;
-
-
-
-
-
-SELECT  count($column) as minus_count, count(*) as count1, minus_count|count1 as percent
- FROM $table
-Where  $column = -1
+https://stackoverflow.com/questions/4741239/select-top-x-or-bottom-percent-for-numeric-values-in-mysql
