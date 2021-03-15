b3156d3b5571d9926d764cbeb64414d77f57c6b6
Ram velagala <67812401+ramvelagala@users.noreply.github.com>
2021-03-14 22:25:13 -0500

    Update main.py
    nabjankjcmabj,hcbakjcsa

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 {"table_name": "M_BFT_FC_TRN_FIX_TERM_MNTY", "column_name": "TRN_FIX_TRM_MTY_SEQ_SK", "data_type": "BIGINT", "is_nullable": true}
-
-
- "no_of_rows": 0, "cardinality": 0, "uniqueness": 0, "num_nulls": 0, "num_non_nulls": 0}
-            
-
-             "tot_rows": 0, "coverage": 0, "value": -1
+This is domain_list.
+{"subject": {"table_name": "M_BFT_FC_TRN_FIX_TERM_MNTY", "column_name": "TRN_FIX_TRM_MTY_SEQ_SK", "data_type": "BIGINT", "is_nullable
+": true}, "no_of_rows": 0, "cardinality": 0, "uniqueness": 0, "num_nulls": 0, "num_non_nulls": 0}
+This is metric_list.
+{"subject": {"table_name": "M_BFT_FC_TRN_FIX_TERM_MNTY", "column_name": "TRN_FIX_TRM_MTY_SEQ_SK", "data_type": "BIGINT", "is_nullable
+": true}, "tot_rows": 0, "coverage": 0, "value": -1}
