1c7a52f6c6a26775de337225760b7f548ca6aea9
Ram velagala <67812401+ramvelagala@users.noreply.github.com>
2021-04-01 13:49:24 -0500

    Update main.py
    click group by working

--- main.py
+++ main.py
@@ -1,9 +1,49 @@
+import click
+
+
+@click.group(invoke_without_command=True)
+@click.option('--age', '-a', type=int)
+@click.pass_context
+def cli(ctx, age):
+    ctx.ensure_object(dict)
+    ctx.obj['age'] = age
+    if ctx.invoked_subcommand is None:
+        click.echo('I was invoked without subcommand')
+        click.echo(f"age: {age}")
+    else:
+        click.echo('I am about to invoke %s' % ctx.invoked_subcommand)
+        click.echo(f"age: {age}")
+
 @click.command()
-@click.option("--name",'-n')
-@click.option("--focus_values", '-f',multiple=True)
-@click.option("--limit_top",prompt="No of Values to be displayed for highest frequent values",type=int)
-def final(name,focus_values, limit_top):
-    print(name)
-    print(f"This is limit_top: {limit_top}")
-    #print(f"This is limit_bot: {limit_bot}")
-    print(f"This is focus_values:{focus_values}")
+@click.option('--profession', '-p')
+@click.pass_context
+def shruthi(ctx,profession):
+    click.echo("Shruthi is working.")
+    click.echo(f"profession: {profession}")
+    click.echo(ctx)
+    click.echo(f'age:{ctx.obj["age"]}')
+
+@click.command()  # @cli, not @click!
+@click.option('--firstname', '-fn')
+@click.pass_context
+def sync(ctx,firstname):
+    click.echo(f'syncing :{firstname}')
+    print(ctx)
+    click.echo('The subcommand')
+
+
+@click.command()  # @cli, not @click!
+@click.option('--lastname', '-ln')
+@click.pass_context
+def desync(ctx,lastname):
+    click.echo(f'desyncing :{lastname}')
+    print(ctx)
+    click.echo('The subcommand')
+
+
+cli.add_command(desync)
+cli.add_command(sync)
+cli.add_command(shruthi)
+
+if __name__ == "__main__":
+    cli(obj={})
