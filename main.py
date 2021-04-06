9b53979f1063067689978039c43051b9438c4690
Ram velagala <67812401+ramvelagala@users.noreply.github.com>
2021-04-05 19:58:58 -0500

    Update main.py
    ansmnkmsacxz

--- main.py
+++ main.py
@@ -1,56 +1,23 @@
-import click
-from datetime import date
+from jinja2 import Environment, PackageLoader
+import os
 
 
-@click.group(invoke_without_command=True)
-@click.option('--age', '-a', type=int)
-@click.option('--speed', prompt="Give speed  in mil/hr")
-@click.option('--date-start','-dt', type=click.DateTime(formats=["%Y-%m-%d"]),default=str(date.today()))
-@click.pass_context
-def cli(ctx, age,speed, date_start):
-    ctx.ensure_object(dict)
-    ctx.obj['age'] = age
-    ctx.obj['speed'] = speed
-    ctx.obj['startdate'] = date_start
-    click.echo(f"start date: {date_start}")
-    if ctx.invoked_subcommand is None:
-        click.echo('I was invoked without subcommand')
-        click.echo(f"age: {age}")
-    else:
-        click.echo('I am about to invoke %s' % ctx.invoked_subcommand)
-        click.echo(f"age: {age}")
+#print(os.path.abspath(__file__))
 
-@click.command()
-@click.option('--profession', '-p')
-@click.pass_context
-def shruthi(ctx,profession):
-    click.echo("Shruthi is working.")
-    click.echo(f"profession: {profession}")
-    click.echo(ctx)
-    click.echo(f'age:{ctx.obj["age"]}')
-    click.echo(f'speed:{ctx.obj["speed"]}')
+env = Environment(loader=PackageLoader('app'))
+template = env.get_template('profiling.html')
 
-@click.command()  # @cli, not @click!
-@click.option('--firstname', '-fn')
-@click.pass_context
-def sync(ctx,firstname):
-    click.echo(f'syncing :{firstname}')
-    print(ctx)
-    click.echo('The subcommand')
-
-
-@click.command()  # @cli, not @click!
-@click.option('--lastname', '-ln')
-@click.pass_context
-def desync(ctx,lastname):
-    click.echo(f'desyncing :{lastname}')
-    print(ctx)
-    click.echo('The subcommand')
-
-
-cli.add_command(desync)
-cli.add_command(sync)
-cli.add_command(shruthi)
-
-if __name__ == "__main__":
-    cli(obj={})
+#print(os.path.abspath(__file__))
+ 
+root = os.path.dirname(os.path.abspath(__file__))
+filename = os.path.join(root, 'html', 'profile.html')
+json_data = os.path.join(root, 'html', 'result.json')
+ 
+with open(filename, 'w') as fh:
+    fh.write(template.render(
+        h1 = "Hello Jinja2",
+        show_one = True,
+        show_two = False,
+        names    = ["Foo", "Bar", "Qux"],
+		json_data = json_data
+    ))
