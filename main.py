1e1038e86b7bc90707eeebef206b8e3900995cda
Ram velagala <67812401+ramvelagala@users.noreply.github.com>
2021-04-07 22:35:55 -0500

    Update main.py
    generate_template.py

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
-from jinja2 import Environment, PackageLoader
-import os
+from jinja2 import Environment, PackageLoader, FileSystemLoader
+import os, json
+from pathlib import Path
+
+# print(os.path.abspath(__file__))
 
 
-#print(os.path.abspath(__file__))
-
-env = Environment(loader=PackageLoader('app'))
-template = env.get_template('profiling.html')
-
-#print(os.path.abspath(__file__))
- 
 root = os.path.dirname(os.path.abspath(__file__))
+template_dir = os.path.join(root, 'app/templates')
+env = Environment(loader=FileSystemLoader(template_dir))
+template = env.get_template('profiling.html')
 filename = os.path.join(root, 'html', 'profile.html')
 json_data = os.path.join(root, 'html', 'result.json')
- 
+domain_metric_data = None
+with open(json_data) as f:
+    data = json.load(f)
+    # print(data, type(data))
+    result = data.get('result')
+    if result:
+        # print(result, type(result))
+        for i in result:
+            rows = i.get('rows')
+            if rows is not None:
+                if rows != 0:
+                    print("no of rows - ", rows)
+                    domain_metric_data = rows
+                else:
+                    print("no of rows - 0")
+
+
 with open(filename, 'w') as fh:
     fh.write(template.render(
-        h1 = "Hello Jinja2",
-        show_one = True,
-        show_two = False,
-        names    = ["Foo", "Bar", "Qux"],
-		json_data = json_data
+        title="Data Profiling",
+        h1="Data Element Profiler",
+        names=["Foo", "Bar", "Qux"],
+        dom_data = domain_metric_data
+        # json_data=json_data
     ))
