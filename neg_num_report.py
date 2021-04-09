e1d9b9a63475ebb79c9dec82544ec09a997e1fe3
Ram velagala <67812401+ramvelagala@users.noreply.github.com>
2021-04-08 21:28:05 -0500

    Update neg_num_report.py
    jnams

--- neg_num_report.py
+++ neg_num_report.py
@@ -1,42 +1,41 @@
-from tests.test_connection import netezza_connection
-from grp_bank_dq_engine_data_element_profiler.profiling_values import SQL_DIR, helper_func
-from grp_bank_dq_engine_data_element_profiler.profiling_classes import DomainMetric,Element
-import pytest
+from jinja2 import Environment, PackageLoader, FileSystemLoader
+import os, json
+from pathlib import Path
 
-@pytest.fixture()
-def whatever(netezza_connection):
-    with netezza_connection.connection as connection:
-        query = connection.sql(SQL_DIR / "main_domain_highlights.sql",
-                               dict(table="M_BFT_FC_TRN_FIX_TERM_MNTY",
-                                    column="TRN_FIX_TRM_MTY_SEQ_K"))
-        return query
-
-@pytest.mark.xfail(raises=ValueError)
-def test_domain_metric_value_sql(whatever):
-    with pytest.raises(Exception):
-        print("Value error has been raised")
-        whatever()
+# print(os.path.abspath(_file_))
 
 
-def test_domain_metric_schema(netezza_connection):
-    with netezza_connection.connection as connection:
-        _,data = next(connection.sql(SQL_DIR / "main_domain_highlights.sql",
-                             dict(table="M_BFT_FC_TRN_FIX_TERM_MNTY",
-                                column="TRN_FIX_TRM_MTY_SEQ_SK")).fillna(0).iterrows())
-        _, meta_data = next(connection.sql(SQL_DIR / "main_meta_data.sql",
-                                           dict(table="M_BFT_FC_TRN_FIX_TERM_MNTY",
-                                                column="TRN_FIX_TRM_MTY_SEQ_SK")).
-                            astype(dict(IS_NULLABLE='bool')).iterrows())
-        meta_data = {k.casefold(): v for k, v in meta_data.to_dict().items() if
-                     v is not None}
-        ele = Element.from_dict(meta_data)
-        print(data)
-        rows = data["rows"]
-        if rows == 0:
-            data["Uniqueness"] = 0
-        else:
-            data["Uniqueness"] = round((data["Cardinality"] * 100 / rows), 4)
-        data = helper_func(data, ele)
-        result = DomainMetric.from_dict(data)
-        print(result)
-        assert result is not None
+root = os.path.dirname(os.path.abspath(_file_))
+template_dir = os.path.join(root, 'app/templates')
+env = Environment(loader=FileSystemLoader(template_dir))
+template = env.get_template('profiling.html')
+filename = os.path.join(root, 'html', 'profile.html')
+json_data = os.path.join(root, 'html', 'result.json')
+domain_metric_data = card = uniq = None
+with open(json_data) as f:
+data = json.load(f)
+  # print(data, type(data))
+    result = data.get('result')
+      if result:
+        # print(result, type(result))
+          for i in result:
+          rows = i.get('rows')
+            if rows is not None:
+              if rows != 0:
+                print("no of rows - ", rows)
+                domain_metric_data = rows
+                card = i.get('cardinality')
+                uniq = i.get('uniqueness')
+                  else:
+                    print("no of rows - 0")
+
+with open(filename, 'w') as fh:
+fh.write(template.render(
+  title="Data Profiling",
+    h1="Data Element Profiler",
+      names=["Foo", "Bar", "Qux"],
+        dom_data = domain_metric_data,
+          cardinality = card,
+            uniqueness = uniq
+              # json_data=json_data
+                ))
