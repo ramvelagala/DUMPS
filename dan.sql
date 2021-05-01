12a3dc9329d38e0253c5eb123045820ff599bcc7
Ram velagala <67812401+ramvelagala@users.noreply.github.com>
2021-05-01 09:53:27 -0500

    Update dan.sql
    initial commit

--- dan.sql
+++ dan.sql
@@ -1,1 +1,28 @@
+DROP TEMPORARY TABLE IF EXIST temp_table;
+CREATE TEMPORARY TABLE temp_table(SELECT * FROM $table LIMIT 0);
 
+PREPARE STMT FROM 'INSERT INTO temp_table SELECT * FROM $table LIMIT ?, 1';
+DELIMITER $$
+DROP PROCEDURE IF EXISTS my_proc$$
+CREATE PROCEDURE my_proc(input INT)
+BEGIN
+    SET @shift = 0;
+    SET @max = 0;
+    SELECT COUNT(*) FROM $table INTO @max;
+
+    my_loop: LOOP
+        IF (input = 0) THEN
+            LEAVE my_loop;
+        END IF;
+        SET input = input - 1;
+		string = concat(string_value,x
+
+        SET @shift = CAST(RAND()*@max AS UNSIGNED);
+        EXECUTE STMT USING @shift;
+
+    END LOOP;
+    SELECT * FROM temp_table;
+END $$
+DELIMITER;
+
+CALL my_proc($row_count)
