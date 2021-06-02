6dcc505beea24843326b5a3b8d5d20236a316624
Ram velagala <67812401+ramvelagala@users.noreply.github.com>
2021-06-02 10:28:16 -0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,1 +1,10 @@
+from ._version import __version__
+
+from . import base
+from .netezza import NetezzaClient
+from .hive import HiveClient
+from .hdfs import HDFSClient
+from .local import LocalClient
+from .snowflake import SnowflakeClient
+from .db2 import DB2Client
 
